21dfeba56e98b58dc6a1b3be452f1487fde82fb1
eliasn <nebiyuelias1@gmail.com>
2022-10-29 09:16:55 +0300

    Setup graphql for django project

---
+++ phison_realestate_backend/utility/set_env_vars.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export DJANGO_READ_DOT_ENV_FILE=True
+# export GOOGLE_APPLICATION_CREDENTIALS='/home/eliasn/work/phison-realestate/phison_realestate_backend/phison-real-estate-firebase-adminsdk.json'
+export GOOGLE_APPLICATION_CREDENTIALS='/home/eliasn/work/phison-realestate/phison_realestate_backend/hamsa-lomi-firebase-adminsdk-43q07-016131b2e9.json'
