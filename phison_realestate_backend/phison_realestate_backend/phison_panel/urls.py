ca87c6040369cdd276472726658ba41ed1044e0b
eliasn <nebiyuelias1@gmail.com>
2022-11-01 21:46:42 +0300

    Create base template for phison_panel app

--- phison_realestate_backend/phison_realestate_backend/phison_panel/urls.py
+++ phison_realestate_backend/phison_realestate_backend/phison_panel/urls.py
@@ -4,4 +4,9 @@ from django.views.generic import TemplateView
 app_name = "phison_panel"
 urlpatterns = [
     path("", TemplateView.as_view(template_name="phison_panel/home.html"), name="home"),
+    path(
+        "property/new/",
+        TemplateView.as_view(template_name="phison_panel/add_property.html"),
+        name="new_property",
+    ),
 ]
