1e9a977101d5736c7a15263d118ecf81da8c2386
eliasn <nebiyuelias1@gmail.com>
2022-11-05 22:23:52 +0300

    Add PropertyForm with tests

--- phison_realestate_backend/phison_realestate_backend/phison_panel/forms.py
+++ phison_realestate_backend/phison_realestate_backend/phison_panel/forms.py
@@ -1,6 +1,22 @@
 from django import forms
 
-from ..core.models import PaymentInformation
+from phison_realestate_backend.core.models import PaymentInformation, Property
+
+
+class PropertyForm(forms.ModelForm):
+    class Meta:
+        model = Property
+        fields = (
+            "name",
+            "bed_room_count",
+            "bath_room_count",
+            "parking_count",
+            "description",
+            "size",
+            "price",
+            "is_featured",
+            "progress",
+        )
 
 
 class PaymentInformationForm(forms.ModelForm):
