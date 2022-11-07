2a921f3723906113b6f46d8c67c13066e153b167
azizadx <azizadx.tech@gmail.com>
2022-11-08 01:25:18 +0300

    constant variables are init

---
+++ phison_realestate_mobile/lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'PHISON',
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Test'),
+          ),
+        ));
+  }
+}
