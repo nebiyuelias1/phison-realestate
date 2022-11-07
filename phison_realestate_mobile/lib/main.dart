47292dc6cb8d740e74db0496dad97dcda37cc77a
Azizadx <45791956+Azizadx@users.noreply.github.com>
2022-11-07 09:11:53 +0300

    Firebase ios is added with patch (#30)
    * mobile init commit
    
    * lefthook is been setup
    
    * android firebase is setup
    
    * ios firebase is setup
    
    * ios firebase is setup patch-1
    
    Co-authored-by: Nebiyu <nebiyuelias1@gmail.com>

--- phison_realestate_mobile/lib/main.dart
+++ phison_realestate_mobile/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
