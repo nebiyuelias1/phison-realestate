0e2a3992f0c51f76a03c7b6dd4af9b97a3a51083
azizadx <azizadx.tech@gmail.com>
2022-11-08 04:13:03 +0300

    theme is added

--- phison_realestate_mobile/lib/presentation/pages/welcome_page.dart
+++ phison_realestate_mobile/lib/presentation/pages/welcome_page.dart
@@ -1,10 +1,10 @@
-//Flutter imports:
+// Flutter imports:
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+// import 'package:flutter_svg/svg.dart';
 
-//project imports:
+//Project imports:
 import '../constants/app_assets_constant.dart';
-import '../constants/app_string_constant.dart';
+// import '../constants/app_string_constant.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -17,17 +17,17 @@ class WelcomePage extends StatelessWidget {
         image: DecorationImage(
             image: AssetImage(PhisonImages.welcomeImagebg), fit: BoxFit.cover),
       ),
-      child: Column(
-        children: [
-          //1st child = logo
-          //2nd child = slong
-          //3rd child = heading
-          //4th child = sentence
-          //5th child = Btn1
-          //6th child = break line
-          //7th child = Btn2
-        ],
-      ),
+      // child: Column(
+      //   children: [
+      //     //1st child = logo
+      //     //2nd child = slong
+      //     //3rd child = heading
+      //     //4th child = sentence
+      //     //5th child = Btn1
+      //     //6th child = break line
+      //     //7th child = Btn2
+      //   ],
+      // ),
     );
   }
 }
