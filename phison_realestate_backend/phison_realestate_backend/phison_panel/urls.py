1421902f5407ea744cbcc976ac4261e7ad5726e9
eliasn <nebiyuelias1@gmail.com>
2022-11-10 09:29:29 +0300

    Make sidebar links active/inactive

--- phison_realestate_backend/phison_realestate_backend/phison_panel/urls.py
+++ phison_realestate_backend/phison_realestate_backend/phison_panel/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from .views import PropertyCreateView, PropertyListView
 
@@ -9,5 +10,10 @@ urlpatterns = [
         "property/new/",
         PropertyCreateView.as_view(),
         name="new_property",
+    ),
+    path(
+        "buyers/",
+        TemplateView.as_view(template_name="phison_panel/buyer_list.html"),
+        name="buyer_list",
     ),
 ]
