0a1413d74795888cdea6089e9e6c3e15fcedd45e
eliasn <nebiyuelias1@gmail.com>
2022-11-16 17:03:27 +0300

    Add ajax view for listing non-staff members

--- phison_realestate_backend/phison_realestate_backend/users/views.py
+++ phison_realestate_backend/phison_realestate_backend/users/views.py
@@ -1,11 +1,39 @@
+import json
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.db.models import Q
+from django.http import JsonResponse
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import DetailView, RedirectView, UpdateView
+from django.views.generic.base import View
+
+from phison_realestate_backend.users.api.serializers import UserSerializer
+
+from ..core.mixins import StaffMemberRequiredMixin
 
 User = get_user_model()
+
+
+class NonStaffMemberListAjaxView(StaffMemberRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        queryset = User.objects.filter(is_superuser=False, is_staff=False).all()
+        query_param = self.request.GET.get("q", "")
+        if query_param:
+            queryset = queryset.filter(
+                Q(name__icontains=query_param)
+                | Q(email__icontains=query_param)
+                | Q(phone_number__icontains=query_param)
+            )
+
+        serializer = UserSerializer(queryset, many=True, context={"request": request})
+        json_data = json.dumps(serializer.data)
+        return JsonResponse(data=json_data, safe=False)
+
+
+non_staff_member_list_ajax_view = NonStaffMemberListAjaxView.as_view()
 
 
 class UserDetailView(LoginRequiredMixin, DetailView):
