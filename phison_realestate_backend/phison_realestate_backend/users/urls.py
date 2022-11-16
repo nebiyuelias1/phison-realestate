ea8933cafb7bd6496ffe948ea6910848c9e5c8aa
eliasn <nebiyuelias1@gmail.com>
2022-11-16 20:55:53 +0300

    Chore work in urls.py

--- phison_realestate_backend/phison_realestate_backend/users/urls.py
+++ phison_realestate_backend/phison_realestate_backend/users/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
     path("<str:username>/", view=user_detail_view, name="detail"),
-    path("ajax/users/", non_staff_member_list_ajax_view, name="ajax_user_list"),
+    path("ajax/customers/", non_staff_member_list_ajax_view, name="ajax_user_list"),
 ]
