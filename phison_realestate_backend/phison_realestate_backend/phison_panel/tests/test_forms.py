f905673dee4c0cabe8bc1d5634b82cee355b044a
eliasn <nebiyuelias1@gmail.com>
2022-11-17 09:53:12 +0300

    Add BuyerPaymentScheduleForm with tests

--- phison_realestate_backend/phison_realestate_backend/phison_panel/tests/test_forms.py
+++ phison_realestate_backend/phison_realestate_backend/phison_panel/tests/test_forms.py
@@ -2,6 +2,7 @@ import pytest
 
 from phison_realestate_backend.phison_panel.forms import (
     BuyerForm,
+    BuyerPaymentScheduleForm,
     PaymentInformationForm,
     PropertyForm,
 )
@@ -134,3 +135,29 @@ class TestBuyerForm:
         form = self.Form(data)
         assert form.is_valid() is False
         assert form.errors != {}
+
+
+class TestBuyerPaymentScheduleForm:
+    def setup(self):
+        self.data = {
+            "title": "test title",
+            "percentage": 23.0,
+            "deadline": "2023-06-04 15:23:08.367007+00:00",
+            "description": "test description",
+        }
+        self.Form = BuyerPaymentScheduleForm
+        self.required_error_message = "This field is required."
+
+    def test_missing_fields(self):
+        for key in self.data.keys():
+            data = {**self.data}
+            del data[key]
+
+            form = BuyerPaymentScheduleForm(data=data)
+            assert form.is_valid() is False
+            assert form.errors[key][0] == self.required_error_message
+
+    def test_valid_form(self):
+        form = self.Form(self.data)
+        assert form.is_valid()
+        assert form.errors == {}
