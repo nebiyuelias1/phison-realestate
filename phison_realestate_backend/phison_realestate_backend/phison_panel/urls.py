c8d372468d4637cc313f47c5f844f2f407e3c823
eliasn <nebiyuelias1@gmail.com>
2022-11-19 21:33:11 +0300

    Implement staff list UI

--- phison_realestate_backend/phison_realestate_backend/phison_panel/urls.py
+++ phison_realestate_backend/phison_realestate_backend/phison_panel/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from .views import (
     BuyerCreateView,
@@ -34,6 +35,14 @@ buyer_urls = [
     ),
 ]
 
+staff_urls = [
+    path(
+        "staff/",
+        TemplateView.as_view(template_name="phison_panel/staff_list.html"),
+        name="staff_list",
+    ),
+]
+
 render_partial_urls = [
     path(
         "render_partial/<partial>/",
@@ -42,4 +51,4 @@ render_partial_urls = [
     ),
 ]
 
-urlpatterns = property_urls + buyer_urls + render_partial_urls
+urlpatterns = property_urls + buyer_urls + render_partial_urls + staff_urls
