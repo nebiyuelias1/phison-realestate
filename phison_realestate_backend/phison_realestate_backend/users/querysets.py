acce03af924063e097692ad122df45f2e62fc9d3
eliasn <nebiyuelias1@gmail.com>
2022-11-22 21:01:18 +0300

    Allow searching and filtering staff members

--- phison_realestate_backend/phison_realestate_backend/users/querysets.py
+++ phison_realestate_backend/phison_realestate_backend/users/querysets.py
@@ -7,3 +7,10 @@ class UserQuerySet(models.QuerySet):
 
     def get_non_staff_members(self):
         return self.filter(is_staff=False, is_superuser=False)
+
+    def search(self, key):
+        return self.filter(
+            models.Q(name__icontains=key)
+            | models.Q(email__icontains=key)
+            | models.Q(phone_number__icontains=key)
+        )
