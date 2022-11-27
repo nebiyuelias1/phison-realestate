b93215f88a62ef62a9057e77c1c95d9e1f0ec972
eliasn <nebiyuelias1@gmail.com>
2022-11-27 21:18:10 +0300

    Add EditPropertyView

--- phison_realestate_backend/phison_realestate_backend/phison_panel/urls.py
+++ phison_realestate_backend/phison_realestate_backend/phison_panel/urls.py
@@ -7,6 +7,7 @@ from .views import (
     BuyerListView,
     PropertyCreateView,
     PropertyDetailView,
+    PropertyEditView,
     PropertyListAjaxView,
     PropertyListView,
     UploadPropertyImageView,
@@ -18,7 +19,7 @@ app_name = "phison_panel"
 property_urls = [
     path("", PropertyListView.as_view(), name="home"),
     path(
-        "properties/new/",
+        "property/new/",
         PropertyCreateView.as_view(),
         name="new_property",
     ),
@@ -26,6 +27,11 @@ property_urls = [
         "property/detail/<slug:slug>/",
         PropertyDetailView.as_view(),
         name="property_detail",
+    ),
+    path(
+        "property/edit/<slug:slug>/",
+        PropertyEditView.as_view(),
+        name="property_edit",
     ),
     path("ajax/properties/", PropertyListAjaxView.as_view(), name="ajax_property_list"),
     path(
