12e804aab3ca9a535e5241aee53f6f525cce0ff0
eliasn <nebiyuelias1@gmail.com>
2022-11-29 15:22:02 +0300

    Add PropertyTypeOptionsMixin

--- phison_realestate_backend/phison_realestate_backend/phison_panel/mixins.py
+++ phison_realestate_backend/phison_realestate_backend/phison_panel/mixins.py
@@ -1,4 +1,6 @@
-from phison_realestate_backend.core.models import PropertyImage
+from typing import Any
+
+from phison_realestate_backend.core.models import Property, PropertyImage
 from phison_realestate_backend.phison_panel.forms import (
     PaymentInformationFormSet,
     PropertyImageIdFormSet,
@@ -23,3 +25,12 @@ class SavePropertyImageMixin:
     def save_property_images(self, property_image_ids):
         property_images = PropertyImage.objects.filter(id__in=property_image_ids)
         property_images.update(property=self.object)
+
+
+class PropertyTypeOptionsMixin:
+    def get_context_data(self, **kwargs: Any):
+        data = super().get_context_data(**kwargs)
+
+        data["options"] = Property.PROPERTY_TYPES
+
+        return data
