4ccd4c6d4516f10aa43027b43ccd17cc9765e55c
eliasn <nebiyuelias1@gmail.com>
2022-12-02 16:56:14 +0300

    Add BuyerDetailView

--- phison_realestate_backend/phison_realestate_backend/phison_panel/views.py
+++ phison_realestate_backend/phison_realestate_backend/phison_panel/views.py
@@ -5,7 +5,7 @@ from typing import Any
 from django.contrib.auth import get_user_model
 from django.contrib.messages.views import SuccessMessageMixin
 from django.db import models
-from django.db.models import OuterRef, Q, Subquery
+from django.db.models import Q
 from django.forms import BaseForm
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
@@ -88,12 +88,6 @@ class PropertyListView(StaffMemberRequiredMixin, PaginateMixin, ListView):
         data["buyer_count"] = Buyer.objects.count()
 
         return data
-
-    def get_queryset(self) -> models.QuerySet[Property]:
-        property_images = PropertyImage.objects.filter(property=OuterRef("pk"))
-        return Property.objects.annotate(
-            property_image=Subquery(property_images.values("image")[:1])
-        )
 
 
 class PropertyCreateView(
