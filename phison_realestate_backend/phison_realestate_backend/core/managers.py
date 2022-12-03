6e21349397445917193bc0baa8d6290affa29390
eliasn <nebiyuelias1@gmail.com>
2022-12-03 11:38:07 +0300

    Add a BuyerManager for Buyer model

--- phison_realestate_backend/phison_realestate_backend/core/managers.py
+++ phison_realestate_backend/phison_realestate_backend/core/managers.py
@@ -14,3 +14,38 @@ class PropertyManager(models.Manager):
                 property_image=models.Subquery(property_images.values("image")[:1])
             )
         )
+
+
+class BuyerManager(models.Manager):
+    def get_queryset(self):
+        from .models import BuyerPaymentSchedule
+
+        payment_schedule = (
+            BuyerPaymentSchedule.objects.filter(buyer=models.OuterRef("pk"))
+            .order_by()
+            .values("status")
+        )
+        complete_payment_schedule = payment_schedule.filter(
+            status=BuyerPaymentSchedule.COMPLETE
+        )
+
+        payment_schedule_count = payment_schedule.annotate(
+            count=models.Count("status")
+        ).values("count")
+        complete_payment_schedule_count = complete_payment_schedule.annotate(
+            count=models.Count("status")
+        ).values("count")
+
+        return (
+            super()
+            .get_queryset()
+            .annotate(
+                payment_schedule_count=models.Subquery(payment_schedule_count),
+                complete_payment_schedule_count=complete_payment_schedule_count,
+                completed_percentage=(
+                    models.F("complete_payment_schedule_count")
+                    / models.F("payment_schedule_count")
+                    * 100
+                ),
+            )
+        )
