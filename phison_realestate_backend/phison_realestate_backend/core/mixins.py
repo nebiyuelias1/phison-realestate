cc2db287ab9fb1b9cbeed789ea3d4f3a0eb35acb
eliasn <nebiyuelias1@gmail.com>
2022-12-15 16:57:46 +0300

    class StaffMemberRequiredMixin

--- phison_realestate_backend/phison_realestate_backend/core/mixins.py
+++ phison_realestate_backend/phison_realestate_backend/core/mixins.py
@@ -5,7 +5,7 @@ from django.contrib.auth.mixins import UserPassesTestMixin
 
 class StaffMemberRequiredMixin(UserPassesTestMixin):
     def test_func(self) -> Optional[bool]:
-        return self.request.user.is_staff
+        return self.request.user.is_staff or self.request.user.is_superuser
 
 
 class PaginateMixin:
