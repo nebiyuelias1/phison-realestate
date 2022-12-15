cd1c448ced8934e121b1cb6dc7514b985b410b64
eliasn <nebiyuelias1@gmail.com>
2022-12-15 11:03:31 +0300

    Fix null access error

--- phison_realestate_backend/phison_realestate_backend/static/js/project.js
+++ phison_realestate_backend/phison_realestate_backend/static/js/project.js
@@ -7,17 +7,16 @@ closeModalButton.addEventListener("click", () => {
 });
 
 // TODO: Find a better way of closing the modal dialog when the outside area is clicked.
-
 const menuButton = document.querySelector('#menuButton');
 const closeMenuButton = document.querySelector('#closeMenuButton');
 const sideBar = document.querySelector('#sideBar');
 
-menuButton.addEventListener('click', () => {
+menuButton?.addEventListener('click', () => {
   sideBar.classList.remove('hidden');
   sideBar.classList.add('p-4', 'flex', 'flex-col');
 });
 
-closeMenuButton.addEventListener('click', () => {
+closeMenuButton?.addEventListener('click', () => {
   sideBar.classList.add('hidden');
   sideBar.classList.remove('p-4');
 });
