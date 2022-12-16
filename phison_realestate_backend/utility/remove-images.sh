b7a830ebd12ca93aa47d4f29d779c4ea15df5280
Nebiyu <nebiyuelias1@gmail.com>
2022-12-17 00:12:52 +0300

    Update remove-images.sh

--- phison_realestate_backend/utility/remove-images.sh
+++ phison_realestate_backend/utility/remove-images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source /home/phisonrealestate/virtualenv/repositories/phison-realestate/phison_realestate_backend/3.8/bin/activate && cd /home/phisonrealestate/repositories/phison-realestate/phison_realestate_backend
-python manage.py removeimages
+python manage.py removeimages --settings config.settings.production
