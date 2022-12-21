da27637909bfe52d4159a5fe708211517a36f483
eliasn <nebiyuelias1@gmail.com>
2022-12-21 16:52:00 +0300

    Add notification model

--- phison_realestate_backend/phison_realestate_backend/core/admin.py
+++ phison_realestate_backend/phison_realestate_backend/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import BuyerPaymentSchedule, Property
+from .models import BuyerPaymentSchedule, Notification, Property
 
 admin.site.register(Property)
 admin.site.register(BuyerPaymentSchedule)
+admin.site.register(Notification)
