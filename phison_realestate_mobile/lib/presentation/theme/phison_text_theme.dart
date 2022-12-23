072372cfae66a5d3b9e868d6822d40be5f692e55
eliasn <nebiyuelias1@gmail.com>
2022-12-23 22:15:04 +0300

    Add phison theme

--- phison_realestate_mobile/lib/presentation/theme/phison_text_theme.dart
+++ phison_realestate_mobile/lib/presentation/theme/phison_text_theme.dart
@@ -1,24 +1,19 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
 
-// Project imports:
-import '../constants/app_assets_constant.dart';
-
-final TextTheme phisonTextTheme = TextTheme(
-  displayLarge: const TextStyle(
-      fontSize: 24,
-      fontWeight: FontWeight.w700,
-      color: PhisonColors.lightBackground),
-  titleLarge: TextStyle(
-      fontSize: 20,
-      fontWeight: FontWeight.w700,
-      color: PhisonColors.primarySwatch[900]),
-  titleMedium: const TextStyle(
-      fontSize: 18,
-      fontWeight: FontWeight.w500,
-      color: PhisonColors.drakGrayTextColor),
-  // button: const TextStyle(
-  //     fontSize: 16,
-  //     fontWeight: FontWeight.w600,
-  //     color: PhisonColors.lightBackground)
+const phisonTextTheme = TextTheme(
+  headline1: TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  ),
+  bodyText1: TextStyle(
+    fontSize: 16,
+  ),
+  bodyText2: TextStyle(
+    fontSize: 14,
+  ),
+  button: TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+  ),
 );
