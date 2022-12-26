880de37d71c7407327d1996ed980d3627e8827c4
eliasn <nebiyuelias1@gmail.com>
2022-12-26 17:39:46 +0300

    Add reusable app bar widget

---
+++ phison_realestate_mobile/lib/shared/widgets/phison_app_bar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+AppBar getAppBar({required BuildContext context, required String title}) {
+  return AppBar(
+    elevation: 0.0,
+    leading: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey.shade200,
+          borderRadius: BorderRadius.circular(32.0),
+        ),
+        child: IconButton(
+          icon: const Icon(Icons.chevron_left),
+          color: Colors.black,
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+        ),
+      ),
+    ),
+    title: Text(title),
+  );
+}
