ca3a3191c743f39f8b3081c69acb017585930ca9
eliasn <nebiyuelias1@gmail.com>
2022-12-28 10:19:54 +0300

    Implement UI for app bar

--- phison_realestate_mobile/lib/pages/home_page/view/home_page_app_bar.dart
+++ phison_realestate_mobile/lib/pages/home_page/view/home_page_app_bar.dart
@@ -1,9 +1,51 @@
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:phison_realestate_mobile/presentation/constants/app_assets_constant.dart';
 
 AppBar getHomePageAppBar() {
   return AppBar(
     toolbarHeight: 72.0,
+    titleSpacing: 0,
+    leading: Stack(
+      alignment: Alignment.center,
+      children: [
+        Positioned(
+          top: -292,
+          left: -175,
+          child: Transform.rotate(
+            angle: math.pi / 10,
+            child: Container(
+              width: 330,
+              height: 330,
+              decoration: BoxDecoration(
+                color: PhisonColors.orange.shade100.withOpacity(0.8),
+                borderRadius: BorderRadius.circular(32.0),
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: -290,
+          left: -82,
+          child: Transform.rotate(
+            angle: math.pi / 7,
+            child: Container(
+              width: 250,
+              height: 330,
+              decoration: BoxDecoration(
+                color: PhisonColors.purple.shade100.withOpacity(0.8),
+                borderRadius: BorderRadius.circular(32.0),
+              ),
+            ),
+          ),
+        ),
+        Image.asset(
+          'assets/images/phison-logo.png',
+          scale: 1.2,
+        ),
+      ],
+    ),
+    leadingWidth: 200,
     actions: [
       Row(
         children: [
