24c6abd2aea4eac3e110e4f69e5d5149344ed26e
eliasn <nebiyuelias1@gmail.com>
2023-01-04 20:48:59 +0300

    Improve app bar property detail page

--- phison_realestate_mobile/lib/shared/widgets/phison_app_bar.dart
+++ phison_realestate_mobile/lib/shared/widgets/phison_app_bar.dart
@@ -10,23 +10,34 @@ AppBar getAppBar({
     automaticallyImplyLeading: false,
     leading: hideLeading
         ? null
-        : Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.grey.shade200,
-                borderRadius: BorderRadius.circular(32.0),
-              ),
-              child: IconButton(
-                icon: const Icon(Icons.chevron_left),
-                color: Colors.black,
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                tooltip: MaterialLocalizations.of(context).backButtonTooltip,
-              ),
-            ),
-          ),
+        : const AppBarLeadingIcon(),
     title: Text(title),
   );
+}
+
+class AppBarLeadingIcon extends StatelessWidget {
+  const AppBarLeadingIcon({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.grey.shade200,
+            borderRadius: BorderRadius.circular(32.0),
+          ),
+          child: IconButton(
+            icon: const Icon(Icons.chevron_left),
+            color: Colors.black,
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            tooltip: MaterialLocalizations.of(context).backButtonTooltip,
+          ),
+        ),
+      );
+  }
 }
