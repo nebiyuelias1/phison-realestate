7f4208ac8a4e3d52ab5bb8ec20859c4e5c0be5f5
eliasn <nebiyuelias1@gmail.com>
2023-01-08 21:32:05 +0300

    Show bottom modal sheet for changing language

--- phison_realestate_mobile/lib/pages/profile/view/profile_page.dart
+++ phison_realestate_mobile/lib/pages/profile/view/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:phison_realestate_mobile/pages/profile/view/change_language_bottom_sheet.dart';
 import 'package:phison_realestate_mobile/presentation/constants/app_assets_constant.dart';
 import 'package:phison_realestate_mobile/shared/widgets/phison_app_bar.dart';
 
@@ -17,40 +18,51 @@ class ProfilePage extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: SingleChildScrollView(
           child: Column(
-            children: const [
-              _ProfilePhoto(),
-              SizedBox(
+            children: [
+              const _ProfilePhoto(),
+              const SizedBox(
                 height: 8.0,
               ),
-              _ProfileItem(
+              const _ProfileItem(
                 label: '+251912345678',
                 icon: Icons.phone_outlined,
               ),
-              SizedBox(
+              const SizedBox(
                 height: 8.0,
               ),
-              _ProfileItem(
+              const _ProfileItem(
                 label: 'Jhon Doe',
                 icon: Icons.person_outline,
               ),
-              SizedBox(
+              const SizedBox(
                 height: 8.0,
               ),
-              _ProfileItem(
+              const _ProfileItem(
                 label: 'jhondoe@gmail.com',
                 icon: Icons.email_outlined,
               ),
-              SizedBox(
+              const SizedBox(
                 height: 8.0,
               ),
               _ProfileItem(
                 label: 'Change Language',
                 icon: Icons.language,
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(32.0),
+                      ),
+                    ),
+                    builder: (context) => const ChangeLanguageBottomSheet(),
+                  );
+                },
               ),
-              SizedBox(
+              const SizedBox(
                 height: 16.0,
               ),
-              _LogoutButton()
+              const _LogoutButton()
             ],
           ),
         ),
@@ -82,31 +94,37 @@ class _ProfilePhoto extends StatelessWidget {
 class _ProfileItem extends StatelessWidget {
   final String label;
   final IconData icon;
+  final VoidCallback? onTap;
+
   const _ProfileItem({
     required this.label,
     required this.icon,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16.0),
-        color: Colors.grey.shade100,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            label,
-            style: const TextStyle(
-              fontWeight: FontWeight.w500,
-              color: Colors.black,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16.0),
+          color: Colors.grey.shade100,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              label,
+              style: const TextStyle(
+                fontWeight: FontWeight.w500,
+                color: Colors.black,
+              ),
             ),
-          ),
-          Icon(icon),
-        ],
+            Icon(icon),
+          ],
+        ),
       ),
     );
   }
