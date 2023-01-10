38d6c04468ad6b156bc35355af0e0147ad9ee3b9
eliasn <nebiyuelias1@gmail.com>
2023-01-10 15:32:20 +0300

    Add multiple starting points

---
+++ phison_realestate_mobile/lib/main_dev.dart
@@ -0,0 +1,28 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_flavor/flutter_flavor.dart';
+import 'package:phison_realestate_mobile/repositories/authentication_repository/authentication_repository.dart';
+
+// Project imports:
+import 'pages/app/view/app.dart';
+
+Future<void> main() async {
+  FlavorConfig(
+    variables: {
+      "baseUrl": "http://localhost:8000",
+    },
+  );
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  final authenticationRepository = AuthenticationRepository();
+  await authenticationRepository.user.first;
+
+  runApp(App(
+    authenticationRepository: authenticationRepository,
+  ));
+}
