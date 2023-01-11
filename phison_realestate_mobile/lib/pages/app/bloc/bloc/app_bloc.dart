23fd1072609c9045b1448ddb17d0f859bb4f422a
eliasn <nebiyuelias1@gmail.com>
2023-01-12 00:03:28 +0300

    Add app bloc

---
+++ phison_realestate_mobile/lib/pages/app/bloc/bloc/app_bloc.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../repositories/authentication_repository/authentication_repository.dart';
+import '../../../../repositories/authentication_repository/models/user.dart';
+
+part 'app_event.dart';
+part 'app_state.dart';
+
+class AppBloc extends Bloc<AppEvent, AppState> {
+  AppBloc({required AuthenticationRepository authenticationRepository})
+      : _authenticationRepository = authenticationRepository,
+        super(
+          authenticationRepository.currentUser.isNotEmpty
+              ? AppState.authenticated(authenticationRepository.currentUser)
+              : const AppState.unauthenticated(),
+        ) {
+    _userSubscription = _authenticationRepository.user.listen(_onUserChanged);
+    on<AppUserChanged>((event, emit) {
+      emit(_mapUserChangedToState(event, state));
+    });
+    on<AppLogoutRequested>((event, emit) async {
+      await _authenticationRepository.logOut();
+    });
+  }
+
+  final AuthenticationRepository _authenticationRepository;
+  late final StreamSubscription<User> _userSubscription;
+
+  void _onUserChanged(User user) => add(AppUserChanged(user));
+
+  AppState _mapUserChangedToState(AppUserChanged event, AppState state) {
+    return event.user.isNotEmpty
+        ? AppState.authenticated(event.user)
+        : const AppState.unauthenticated();
+  }
+
+  @override
+  Future<void> close() {
+    _userSubscription.cancel();
+    return super.close();
+  }
+}
