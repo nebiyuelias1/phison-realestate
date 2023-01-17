4ef0d8c7ec02cfad2b7b77b053820b65c4f17f5a
eliasn <nebiyuelias1@gmail.com>
2023-01-17 22:22:23 +0300

    Initialize firebase app with certificate from json string

--- phison_realestate_backend/utility/set_env_vars.sh
+++ phison_realestate_backend/utility/set_env_vars.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 export DJANGO_READ_DOT_ENV_FILE=True
-export GOOGLE_APPLICATION_CREDENTIALS='/home/eliasn/work/phison-realestate/phison_realestate_backend/phison-real-estate-firebase-adminsdk.json'
