da46df7b548c52decf7041464c68f984414085a6
Nebiyu <nebiyuelias1@gmail.com>
2023-09-06 19:20:06 +0800

    Set default value for DJANGO_SETTINGS_MODULE

--- phison_realestate_backend/manage.py
+++ phison_realestate_backend/manage.py
@@ -4,7 +4,9 @@ import sys
 from pathlib import Path
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+    # Check if DJANGO_SETTINGS_MODULE is already set in the environment
+    if "DJANGO_SETTINGS_MODULE" not in os.environ:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
     try:
         from django.core.management import execute_from_command_line
